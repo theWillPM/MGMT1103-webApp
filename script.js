e800dee66f9cc25fb9ffa7f32b6c42e9957a3cf6
Willian P. Munhoz <96987268+theWillPM@users.noreply.github.com>
2022-11-27 08:16:27 -0700

    I don`t remember by now. Slept on top of the keyboard

--- script.js
+++ script.js
@@ -4,18 +4,29 @@ let teamsHomeElements = document.querySelectorAll('.you, .statusIcon, .chatLine,
 let chatTop = document.getElementById("chatTop");
 let currentTab = document.getElementById("activeTab");
 let main = document.querySelector("main");
-let yourChat = document.querySelectorAll('.yourChat');
+let yourChatYou = document.querySelectorAll('.yourChatContainer.your, .timeSent.your, .yourChat.your');
+let yourChatWade = document.querySelectorAll('.yourChatContainer.wade, .timeSent.wade, .yourChat.wade');
+let allChat = document.querySelectorAll('yourChatContainer, .theirChatContainer, .yourChat, .theirChat, .timeSent');
 let chatBottom = document.querySelector('.chatBottom');
 let footerMenu = document.querySelector('#footer');
-let timesent = document.querySelector('.timeSent');
 let activeTab = "home";
 let backButton = document.getElementById("back");
 let navbar = document.getElementById("navbar");
 let homeScreen = document.getElementById("homeScreen");
 let bvcTeamsIcon = document.getElementById("bvcTeams");
 let teamsIcon = document.getElementById("teamsIcon");
-
-console.log(chatBottom);
+let homeButton = document.getElementById("homeButtons");
+let wadeChat = document.getElementById("wadeChat");
+let meetingChat = document.getElementById("meetingChat");
+let currentProfPic = document.getElementById("currentProfPic");
+let yourProfPic = document.getElementById("yourChatPic");
+let chatScreen = document.getElementById("chatScreen");
+let theirChatWade = document.querySelectorAll('.theirChatContainer.wade, .theirChat.wade');
+let profileIconBarContainer = document.getElementById("profileIconBarContainer");
+let profileContent = document.querySelectorAll('#email, #emailContainer, #profileIconBarContainer, #profileIconBar, .badges');
+let profName = document.querySelector('figcaption');
+let currentEmail = document.getElementById('email');
+console.log(yourChatYou);
 
 goHome();
 
@@ -27,73 +38,153 @@ function hideBanner() {
 }
 
 function goToYourChat() {
-    teamsHomeElements.forEach(element => {
-        element.style.display = "none";
-    });
-    chatTop.style.display = "flex";
-    currentTab.innerText = "Student (You)";
-    currentTab.className += " profileLink";
+    if (activeTab!="yourProfile") 
+    {
+        teamsHomeElements.forEach(element => {
+            element.style.display = "none";
+        });
+        chatTop.style.display = "flex";
+        currentTab.innerText = "Student (You)";
+        currentTab.className += " yourProfileLink";
+        chatScreen.style.display = "flex";
+        
+        main.style.backgroundColor = "var(--color5)";
 
-     
-    main.style.backgroundColor = "black";
-    yourChat.forEach(element => {
-        element.style.display = "flex";
-    });
-    footerMenu.style.display = "none";
-    chatBottom.style.display = "flex";
-    timesent.style.display = "flex";
-    activeTab = "you";
+        yourChatYou.forEach(element => {
+            element.style.display = "flex";
+        });
+        profileContent.forEach(element => {
+            element.style.display = "none";
+        });
+        footerMenu.style.display = "none";
+        chatBottom.style.display = "flex";
+        activeTab = "you";
+    }
 }
 
 function goToChat() {
-    navbar.style.display="flex";
+    navbar.removeAttribute('style');
+    chatScreen.removeAttribute('style');
     teamsHomeElements.forEach(element => {
         element.style.display = "flex";
+    });
+    profileContent.forEach(element => {
+        element.style.display = "none";
+    });
+    allChat.forEach(element => {
+        element.style.display = "none";
     });
     homeScreen.style.display="none";
     chatTop.style.display = "none";
     currentTab.innerText = "Chat";
     currentTab.className = "";
-    main.style.backgroundColor = "#303030";
-    yourChat.forEach(element => {
-        element.style.display = "none";
-    });
+    main.style.backgroundColor = "var(--color1)";
+
     footerMenu.style.display = "flex";
     chatBottom.style.display = "none";
-    timesent.style.display = "none";
     activeTab = "chat";
-
+    homeButton.style.display="none";
+    currentProfPic.src="./img/profilePic2.png";
+    chatScreen.style.display="none";   
+    profileIconBarContainer.style.display="none";
+    resetUser();
 }
 
-function goToYourProfile() {
-
+function openYourProfile() {
+    chatTop.style.display = "none";
+    yourProfile.style.display = "flex";
+    navbar.style.display = "block";
+    navbar.style.height = "50px";
+    chatScreen.style.justifyContent = "flex-start";
+    chatScreen.style.alignItems = "flex-start";
+    allChat.forEach(element => {
+        element.style.display = "none";
+    });
+    profileContent.forEach(element => {
+        element.style.display = "flex";
+    });
+    chatBottom.removeAttribute('style');
+    activeTab = "yourProfile";
 }
 
 function goHome() {
+    profileIconBarContainer.style.display="none";
     teamsHomeElements.forEach(element => {
         element.style.display = "none";
     });
-    currentTab.innerText = "Student (You)";
-    currentTab.className += " profileLink";
     navbar.style.display = "none";
-    teamsHomeElements.forEach(element => {
+    yourChatYou.forEach(element => {
         element.style.display = "none";
     });
+    theirChatWade.forEach(element => {
+        element.style.display = "none";
+    });
+    profileContent.forEach(element => {
+        element.style.display = "none";
+    });
+    chatBottom.style.display = "none";
     main.style.maxHeight = "540px";
     main.style.height = "540px";
     footerMenu.style.display = "none";
     homeScreen.style.display = "flex";
-
+    chatScreen.style.display= "none";
+    homeButton.style.display= "flex";
     activeTab = "home";
 }
 
+function goToWadeChat() {
+    
+    goToYourChat();
+    currentTab.innerText = "Wade Wilson";
+    yourChatYou.forEach(element => {
+        element.style.display = "none";
+    });
+    currentProfPic.src="./img/wade.png";
+    theirChatWade.forEach(element => {
+        element.style.display = "flex";
+    });
+    currentTab.className += " wadeProfileLink";
+    yourChatWade.forEach(element => {
+        element.style.display = "flex";
+    });
+    activeTab = "wade";
+}
+
+function openWadeProfile() {
+    openYourProfile();
+    yourProfPic.src="./img/wade.png";
+    profName.textContent="Wade Wilson";
+    currentEmail.textContent="w.wilson999@mybvc.ca";
+
+    activeTab = "wadeProfile";
+}
 
 function backClick() {
-    if (activeTab == "you") {
+    if (activeTab == "you" || activeTab == "wade" || activeTab == "meeting") {
         goToChat();
     } else
     if (activeTab == "chat") {
         goHome();
+    } else
+    if (activeTab == "yourProfile") {
+        goToChat();
+        goToYourChat();
+    }
+    if (activeTab == "wadeProfile") {
+        goToChat();
+        goToWadeChat();
+    }
+}
+
+function openProfile() {
+    if (activeTab == "you") {
+        openYourProfile();
+    } else
+    if (activeTab == "chat") {
+        goToChat();
+    } else
+    if (activeTab == "wade") {
+        openWadeProfile();
     }
 }
 
@@ -107,4 +198,10 @@ function bvcTeamsClick() {
 
 function timeout(func, t) {
     setTimeout(func, t);
+}
+
+function resetUser() {
+    yourProfPic.src="./img/profilePic2.png";
+    profName.textContent="Student (You)";
+    currentEmail.textContent="s.tudent123@mybvc.ca";
 }
